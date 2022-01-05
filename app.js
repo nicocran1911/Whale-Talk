d79bcba163327100a1e9b630c5272f6f18dd1d2c
nicocran1911 <93523661+nicocran1911@users.noreply.github.com>
2022-01-05 11:40:54 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    //  console.log('inputindex = '+ inputIndex);
+    for (let vowel = 0; vowel < vowels.length; vowel++) {
+        // console.log('vowel = ' + vowel);
+        if (input[inputIndex] === vowels[vowel]) {
+            if (input[inputIndex] === 'e') {
+                resultArray.push('ee')
+            } else if (input[inputIndex] === 'u') {
+                resultArray.push('uu');
+            } else {
+                resultArray.push(input[inputIndex]);
+            }
+        }
+    }
+};
+console.log(resultArray.join('').toUpperCase());
